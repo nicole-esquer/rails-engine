dedf044f966f31d4cb343bf85f705d2002ff494a
Nicole Esquer <nicole.esquer5@gmail.com>
2022-09-01 02:13:34 -0700

    feature: merchant restful api endpoints completed

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,12 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
-      resources :merchants, only: [:index, :show]
+      resources :merchants, only: [:index, :show] do
+        resources :items
+      end
+      resources :items do
+      end
     end
   end
-
 end
       
--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -23,7 +23,7 @@ RSpec.describe 'Merchants API' do
       end
    end
 
-   xit 'can get one merchant by its id' do
+   it 'can get one merchant by its id' do
       id = create(:merchant).id
 
       get "/api/v1/merchants/#{id}"
@@ -32,10 +32,30 @@ RSpec.describe 'Merchants API' do
 
       merchant = JSON.parse(response.body, symbolize_names: true)
 
-      expect(merchant).to have_key(:id)
-      expect(merchant[:id]).to eq(id)
+      expect(merchant[:data]).to have_key(:id)
+      expect(merchant[:data][:id]).to be_a(String)
 
-      expect(merchant).to have_key(:name)
-      expect(merchant[:name]).to be_a(String)
+      expect(merchant[:data]).to have_key(:attributes)
+      expect(merchant[:data][:attributes]).to have_key(:name)
+
+      expect(merchant[:data]).to have_key(:type)
+      expect(merchant[:data][:type]).to eq('merchant')
+   end
+
+   it 'can get all merchant items' do
+      merchant_1 = create(:merchant)
+      merchant_2 = create(:merchant)
+      item_1 = create_list(:item, 1, merchant_id: merchant_1.id)
+      item_2 = create_list(:item, 2, merchant_id: merchant_2.id)
+
+      get "/api/v1/merchants/#{merchant_1.id}/items"
+
+      merchant_items = JSON.parse(response.body, symbolize_names: true)
+
+      expect(response).to be_successful
+      expect(merchant_items).to have_key(:data)
+
+      expect(merchant_1.items.count).to eq(1)
+      expect(merchant_1.items.count).to_not eq(2)
    end
 end
